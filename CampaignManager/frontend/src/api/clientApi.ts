0b845548a3da06f6f260a61ad34e5ad7c78430ca
Requu1 <sz.potepa25@gmail.com>
2026-05-04 09:53:08 +0200

    Connection issues fix

--- CampaignManager/frontend/src/api/clientApi.ts
+++ CampaignManager/frontend/src/api/clientApi.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
 export const clientApi = axios.create({
-  baseURL: `${apiUrl}/api`,
+  baseURL: "/api",
   withCredentials: true,
 });
